072cd3eb6d4edfd8958c5826a8eb375b1cb6e38b
Ana Sofia Lima <108680344+anasosl@users.noreply.github.com>
2023-11-23 20:45:36 -0300

    cout, inputs destrinchados

--- main.cpp
+++ main.cpp
@@ -34,18 +34,27 @@ int main()
 {
     ofstream f_out;
     f_out.open("test4.ppm");
+
+    double x, y, z;
     
     point3 origem;
     point3 alvo;
     vec3 cima;
-
-    cin >> origem >> alvo >> cima;
+    
+    cout << "\nPara a inicialização, você precisará colocar 9 doubles (3 para a origem, 3 para o alvo e 3 para determinar o vetor cima)\n";
+    cin >> x >> y >> z;
+    origem = (x, y, z);
+    cin >> x >> y >> z;
+    alvo = (x, y, z);
+    cin >> x >> y >> z;
+    cima = (x, y, z)
     
     Camera cam(origem, alvo, cima);
     int dist;
     int resh;
     int resv;
-    
+
+    cout << "\nAgora, você precisará colocar 3 inteiros (distância da câmera para a tela, resolução horizontal e resolução vertical)\n";
     cin >> dist >> resh >> resv;
     
     double tamx = 1;
@@ -58,20 +67,35 @@ int main()
 
     while(true){
         string type = "";
+        double x, y, z;
         point3 p;
         double r;
         vec3 v1, v2;
-        
+
+        cout << "\nPara cada objeto, coloque sphere ou plane. Se você não quiser adicionar outro objeto, digite end.\n";
         cin >> type;
 
         if (type == "sphere"){
-            cin >> p >> r >> v1;
+            cout << "\nPara a esfera, você precisará colocar 7 doubles (3 para o centro, 1 para o raio e 3 para as cores)\n";
+            cin >> x >> y >> z;
+            p = (x, y, z);
+            cin >> r;
+            cin >> x >> y >> z;
+            v1 = (x, y, z)
             sphere sp(p, r, v1);
             objects.push_back(&sp);
+            
         } else if (type == "plane"){
-            cin >> p >> v1 >> v2;
+            cout << "\nPara o plano, você precisará colocar 9 doubles (3 para o centro, 3 para a normal e 3 para as cores)\n";
+            cin >> x >> y >> z;
+            p = (x, y, z);
+            cin >> x >> y >> z;
+            v1 = (x, y, z);
+            cin >> x >> y >> z;
+            v2 = (x, y, z)
             plane pl(p, v1, v2);
             objects.push_back(&pl);
+            
         } else if (type == "end"){
             break;
         }
