2f34b84b77e0cf997a4c77dc7a81c378ceda5df3
Ana Sofia Lima <108680344+anasosl@users.noreply.github.com>
2023-11-23 20:15:12 -0300

    inputs
    point3 origem
    point3 alvo
    vec3 cima
    int dist
    int resh
    int resv
    type (sphere, plane, end)
    para sphere: point3, double, vec3
    para plane: point3, vec3, vec3

--- main.cpp
+++ main.cpp
@@ -34,28 +34,49 @@ int main()
 {
     ofstream f_out;
     f_out.open("test4.ppm");
-    point3 origem = point3(0, 0, 0);
-    point3 alvo = point3(1, 0, 0);
-    vec3 cima = vec3(0, 1, 0);
+    
+    point3 origem;
+    point3 alvo;
+    vec3 cima;
+
+    cin >> origem >> alvo >> cima;
+    
     Camera cam(origem, alvo, cima);
-    int dist = 1;
-    int resh = 500;
-    int resv = 500;
-    // cin >> ds >> resh >> resv;
+    int dist;
+    int resh;
+    int resv;
+    
+    cin >> dist >> resh >> resv;
+    
     double tamx = 1;
     double tamy = 1;
     vec3 qx = (2 * tamx / (resh - 1)) * cam.u;
     vec3 qy = (2 * tamy / (resv - 1)) * cam.v;
     vec3 canto_inf_esq = dist * cam.w - tamx * cam.u - tamy * cam.v;
 
-    sphere sp(point3(3.5, 0.0, -0.7), 1.5, vec3(200, 50, 0));
-    sphere sp1(point3(7, 0.0, 1.0), 3.0, vec3(200, 250, 0));
-    plane pl(point3(0, 1, 0), vec3(0, 1, 0), vec3(20, 20, 150));
-
     vector<geometricObj *> objects;
-    objects.push_back(&sp);
-    objects.push_back(&sp1);
-    objects.push_back(&pl);
+
+    while(true){
+        string type = "";
+        point3 p;
+        double r;
+        vec3 v1, v2;
+        
+        cin >> type;
+
+        if (type == "sphere"){
+            cin >> p >> r >> v1;
+            sphere sp(p, r, v1);
+            objects.push_back(&sp);
+        } else if (type == "plane"){
+            cin >> p >> v1 >> v2;
+            plane pl(p, v1, v2);
+            objects.push_back(&pl);
+        } else if (type == "end"){
+            break;
+        }
+                    
+    }
 
     f_out << "P3\n";
     f_out << resh << " " << resv << "\n255\n";
