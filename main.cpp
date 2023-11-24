e6ae5afc894fc8b99d6601d009c16979e3e4d5bd
JVMergulho <jvitormergulho@gmail.com>
2023-11-23 21:23:14 -0300

    Inputs corretos

--- main.cpp
+++ main.cpp
@@ -10,9 +10,8 @@ using namespace std;
 
 vec3 color(const ray &r, double dist, vector<geometricObj *> &objects)
 {
-
     vector<pair<double, int>> ts;
-    for (int i = 0; i < objects.size(); i++)
+    for (long long unsigned int i = 0; i < objects.size(); i++)
     {
         geometricObj *obj = objects[i];
         double t = obj->intersect(r);
@@ -36,27 +35,27 @@ int main()
     f_out.open("test4.ppm");
 
     double x, y, z;
-    
+
     point3 origem;
     point3 alvo;
     vec3 cima;
-    
-    cout << "\nPara a inicialização, você precisará colocar 9 doubles (3 para a origem, 3 para o alvo e 3 para determinar o vetor cima)\n";
+
+    cout << "\nPara inicializar, tu precisas colocar 9 doubles (3 para a origem, 3 para o alvo e 3 para determinar o vetor cima)\n";
     cin >> x >> y >> z;
     origem = point3(x, y, z);
     cin >> x >> y >> z;
     alvo = point3(x, y, z);
     cin >> x >> y >> z;
     cima = vec3(x, y, z);
-    
+
     Camera cam(origem, alvo, cima);
     int dist;
     int resh;
     int resv;
 
-    cout << "\nAgora, você precisará colocar 3 inteiros (distância da câmera para a tela, resolução horizontal e resolução vertical)\n";
+    cout << "\nAgora, tu precisas colocar 3 inteiros (distancia da camera para a tela, resolucao horizontal e resolucao vertical)\n";
     cin >> dist >> resh >> resv;
-    
+
     double tamx = 1;
     double tamy = 1;
     vec3 qx = (2 * tamx / (resh - 1)) * cam.u;
@@ -65,41 +64,44 @@ int main()
 
     vector<geometricObj *> objects;
 
-    while(true){
+    while (true)
+    {
         string type = "";
         double x, y, z;
         point3 p;
         double r;
         vec3 v1, v2;
 
-        cout << "\nPara cada objeto, coloque sphere ou plane. Se você não quiser adicionar outro objeto, digite end.\n";
+        cout << "\nPara cada objeto, coloque sphere ou plane. Se tu nao quiseres adicionar outro objeto, digite end.\n";
         cin >> type;
 
-        if (type == "sphere"){
-            cout << "\nPara a esfera, você precisará colocar 7 doubles (3 para o centro, 1 para o raio e 3 para as cores)\n";
+        if (type == "sphere")
+        {
+            cout << "\nPara a esfera, tu precisas colocar 7 doubles (3 para o centro, 1 para o raio e 3 para as cores)\n";
             cin >> x >> y >> z;
             p = point3(x, y, z);
             cin >> r;
             cin >> x >> y >> z;
             v1 = vec3(x, y, z);
-            sphere sp(p, r, v1);
-            objects.push_back(&sp);
-            
-        } else if (type == "plane"){
-            cout << "\nPara o plano, você precisará colocar 9 doubles (3 para o centro, 3 para a normal e 3 para as cores)\n";
+            sphere *sp = new sphere(p, r, v1);
+            objects.push_back(sp);
+        }
+        else if (type == "plane")
+        {
+            cout << "\nPara o plano, tu precisas colocar 9 doubles (3 para o centro, 3 para a normal e 3 para as cores)\n";
             cin >> x >> y >> z;
             p = point3(x, y, z);
             cin >> x >> y >> z;
             v1 = vec3(x, y, z);
             cin >> x >> y >> z;
             v2 = vec3(x, y, z);
-            plane pl(p, v1, v2);
-            objects.push_back(&pl);
-            
-        } else if (type == "end"){
+            plane *pl = new plane(p, v1, v2);
+            objects.push_back(pl);
+        }
+        else if (type == "end")
+        {
             break;
         }
-                    
     }
 
     f_out << "P3\n";
